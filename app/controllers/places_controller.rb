26a2ea3a0ea307341496b85d42e9e573d8ab2604
Angela <chunsaa88@gmail.com>
2017-04-20 04:15:28 +0000

    Added pagination

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,8 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    @places = Place.all.paginate(:page => params[:page], :per_page => 3)
+    @places_last = Place.last
   end
 
 end
