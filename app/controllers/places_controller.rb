1b79245e217e1c93fccac223f1ad013cd1e1342b
Angela <chunsaa88@gmail.com>
2017-05-02 21:51:24 +0000

    fixed geocoding issue and application.html.erb

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -16,7 +16,7 @@ class PlacesController < ApplicationController
 
   def create
     @place = current_user.places.create(place_params)
-    if @place.valid?
+    if @place.save
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
