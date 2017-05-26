cfa2d563aeb61decaf00850f1fa0153f3f66995e
Angela <chunsaa88@gmail.com>
2017-05-26 01:47:42 +0000

    modified place model and added instance methods

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,4 +6,13 @@ class Place < ApplicationRecord
   after_validation :geocode
 
   validates :name, :address, :description, presence: true, length: { minimum: 3 }
+
+  def comment_email
+    comments.last.user.email if comments.last.present? && comments.last.user.email
+  end
+
+  def human_rating
+    comments.last.humanized_rating if comments.last.present? && comments.last.humanized_rating
+  end
+  
 end
