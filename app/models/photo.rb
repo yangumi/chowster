c7218b72b08a766d7d07539a0edf06a25289f41f
Angela <chunsaa88@gmail.com>
2017-06-01 03:07:17 +0000

    updated code for photo upload

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,6 @@
 class Photo < ApplicationRecord
+  belongs_to :place
+  
   def create_photo
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506015559) do
+ActiveRecord::Schema.define(version: 20170531235912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170506015559) do
     t.integer  "user_id"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "picture"
     t.index ["user_id"], name: "index_places_on_user_id", using: :btree
   end
 
